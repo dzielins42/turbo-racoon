a3b0153e47682370c23939049a5e303ae279c7da
d.zielinski <dzielins42@gmail.com>
2017-07-27 11:23:26 +0200

    Add Material Design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { DummyComponent } from './dummy/dummy.component';
+import { MaterialTestComponent } from './material-test/material-test.component';
 
 const routes: Routes = [
   { path: '', component:AppComponent },
-  { path: 'dummy', component:DummyComponent }
+  { path: 'dummy', component:DummyComponent },
+  { path: 'material-test', component:MaterialTestComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DummyComponent } from './dummy/dummy.component';
+import { MaterialTestComponent } from './material-test/material-test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DummyComponent
+    DummyComponent,
+    MaterialTestComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpModule,
+    MaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
